72b71a1e2c56e4bd5889539c601a59851e056e46
Marchewk4 <k.kowalska90@gmail.com>
2024-02-19 21:34:57 +0100

    three columns and fixed home img

--- src/components/Home/HomeThreeColumns.jsx
+++ src/components/Home/HomeThreeColumns.jsx
@@ -3,7 +3,21 @@ import "../../scss/main.scss"
 function HomeThreeColumns() {
 
     return (
-        <div>Home Three Colums</div>
+        <section className="cols">
+            <div className="col">
+                <h1>10</h1>
+                <span>Oddanych worków</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="col">
+                <h1>5</h1>
+                <span>Wspartych organizacji</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>  </div>
+            <div className="col">
+                <h1>7</h1>
+                <span>Zorganizowanych zbiórek</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> </div>
+        </section>
     );
 }
 
