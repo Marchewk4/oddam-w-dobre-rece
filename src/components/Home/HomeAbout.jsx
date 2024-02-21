35ea58251d6652810aef633e016ae67051476994
Marchewk4 <k.kowalska90@gmail.com>
2024-02-21 18:44:08 +0100

    about section and 3 colums fixed

--- src/components/Home/HomeAbout.jsx
+++ src/components/Home/HomeAbout.jsx
@@ -1,9 +1,22 @@
+import Signature from "../../assets/Signature.svg";
+import Decoration from "../../assets/Decoration.svg";
+import AboutImage from "../../assets/People.jpg";
 import "../../scss/main.scss"
 
 function HomeAbout() {
 
     return (
-        <div className="about" name="about">Home About</div>
+        <section className='about' name="about">
+            <div className='left-side-container'>
+                <h2>O nas</h2>
+                <img src={Decoration} alt="decoration"/>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <img src={Signature} alt="signature" className="signature"/>
+            </div>
+            <div className='right-side-container'>
+                <img src={AboutImage} alt="people"/>
+            </div>
+        </section>
     );
 }
 
