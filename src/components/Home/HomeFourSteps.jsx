72ca1114e320dead26844833e5e73e54c514cb2d
Marchewk4 <k.kowalska90@gmail.com>
2024-02-21 17:25:15 +0100

    four steps section

--- src/components/Home/HomeFourSteps.jsx
+++ src/components/Home/HomeFourSteps.jsx
@@ -1,9 +1,54 @@
+import { Link } from 'react-router-dom';
+
+import Decoration from "../../assets/Decoration.svg";
+import Icon1 from "../../assets/Icon-1.svg";
+import Icon2 from "../../assets/Icon-2.svg";
+import Icon3 from "../../assets/Icon-3.svg";
+import Icon4 from "../../assets/Icon-4.svg";
+
+
 import "../../scss/main.scss"
 
 function HomeFourSteps() {
 
     return (
-        <div className="About" name="fourSteps">Home Four Steps</div>
+        <section className="four-steps">
+            <div className="four-steps__title">
+                <h2>Wystarczą 4 proste kroki</h2>
+                <img src={Decoration} alt="decoration" />
+            </div>
+
+            <div className="four-steps__steps">
+                <div className='step'>
+                    <img src={Icon1} alt="icon1"/>
+                    <span>Wybierz rzeczy</span>
+                    <hr/>
+                    <span>ubrania, zabawki,<br/>sprzęt i inne</span>
+                </div>
+                <div className='step'>
+                    <img src={Icon2} alt="icon2"/>
+                    <span>Spakuj je</span>
+                    <hr/>
+                    <span>skorzystaj z<br/>worków na śmieci</span>
+                </div>
+                <div className='step'>
+                    <img src={Icon3} alt="icon3"/>
+                    <span>Zdecyduj komu<br/>chcesz pomóc</span>
+                    <hr/>
+                    <span>wybierz zaufane<br/>miejsce</span>
+                </div>
+                <div className='step'>
+                    <img src={Icon4} alt="icon4"/>
+                    <span>Zamów kuriera</span>
+                    <hr/>
+                    <span>kurier przyjedzie<br/>w dogodnym terminie</span>
+                </div>
+            </div>
+
+            <div className='main-buttons'>
+                <Link className='main-button' to='/login'>ODDAJ<br/>RZECZY</Link>
+            </div>
+        </section>
     );
 }
 
