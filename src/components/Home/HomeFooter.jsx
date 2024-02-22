21a730a8b99abdfcf43626024b05c51a2cda9904
Marchewk4 <k.kowalska90@gmail.com>
2024-02-22 17:09:52 +0100

    footer

--- src/components/Home/HomeFooter.jsx
+++ src/components/Home/HomeFooter.jsx
@@ -1,9 +1,20 @@
-import "../../scss/main.scss"
+import Facebook from "../../assets/Facebook.svg";
+import Instagram from "../../assets/Instagram.svg";
+import "../../scss/main.scss";
 
 function HomeFooter() {
 
     return (
-        <div>Home Footer</div>
+        <footer className="footer">
+            <div/>
+            <div className="copyright">
+                <p>Copyright by Coders Lab</p>
+            </div>
+            <div className='icons'>
+                <a href="#"><img src={Facebook} alt="facebook-icon"/></a>
+                <a href="#"><img src={Instagram} alt="instagram-icon"/></a>
+            </div>
+        </footer>
     );
 }
 
