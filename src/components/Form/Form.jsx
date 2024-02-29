5c92ec88b2aad60947245016cde1506eede5e9d7
Marchewk4 <k.kowalska90@gmail.com>
2024-02-29 18:29:01 +0100

    fixed form main

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,7 +4,6 @@ import FormMain from "./FormMain.jsx";
 import FormImportant from "./FormImportant.jsx";
 import HomeContact from "../Home/HomeContact.jsx";
 import HomeFooter from "../Home/HomeFooter.jsx";
-import FormImage from "../../assets/Form-Hero-Image.jpg";
 import "../../scss/main.scss"
 
 function Form() {
@@ -13,7 +12,6 @@ function Form() {
         <>
             <div className="form-start" name="formStart">
                 <div className="left-side">
-                    {/*<img src={FormImage} alt="form-image" className="form-image"/>*/}
                 </div>
                 <div className="right-side container">
                     <div className="header-nav-container">
